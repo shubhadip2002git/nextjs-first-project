8f07650c5850bc96ccfef7346c35d77a2019631e
Shubhadip <shubhadip2002git@gmail.com>
2025-09-05 01:26:27 +0530

    First Commit

---
+++ src/components/footer.tsx
@@ -0,0 +1,5 @@
+export default function Footer(){
+  return <footer className="mt-auto text-center text-zinc-400 py-5 px-7 border-t">
+    <small>&copy; 2025. All rights reserved</small>
+  </footer>
+}
